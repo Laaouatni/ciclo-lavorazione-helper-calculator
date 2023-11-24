55f31b8ceb9f98a5d9d73688cbef7ffbfa24225e
Laaouatni Anas <laaouatni.anas@outlook.it>
2023-11-24 23:04:31 +0100

    added tempo minute visualization

--- app/src/lib/components/List/Row/RowItem/RowItem.svelte
+++ app/src/lib/components/List/Row/RowItem/RowItem.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import TempoConversione from "./Title/TempoConversione/TempoConversione.svelte";
   import type { TypeParametriLavorazioneIndexNames } from "../utils/types/types";
   import RowItemInput from "./Input/RowItemInput.svelte";
   import RowItemTitle from "./Title/RowItemTitle.svelte";
@@ -6,10 +7,13 @@
   export let keyId: TypeParametriLavorazioneIndexNames;
 </script>
 
-<!-- active:fixed active:top-[50%] active:left-[50%] active:-translate-x-[50%] active:-translate-y-[50%] active:z-10 active:w-[80%] active:outline active:outline-[100vmin] active:outline-neutral-900/90 active:isolate -->
 <div
-  class="border-2 dark:border-neutral-500 grid rounded-md overflow-hidden hover:scale-[0.98] transition"
+  class="border-2 dark:border-neutral-500 grid rounded-md overflow-hidden hover:scale-[0.98] active:shadow-xl active:dark:shadow-neutral-900 transition duration-500 active:border-l-8"
 >
-  <RowItemTitle {keyId}></RowItemTitle>
+  <RowItemTitle {keyId}>
+    {#if keyId == "tempo"}
+      <TempoConversione/>
+    {/if}
+  </RowItemTitle>
   <RowItemInput {keyId}></RowItemInput>
 </div>
---
+++ app/src/lib/components/List/Row/RowItem/Title/TempoConversione/TempoConversione.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { parametriLavorazioneStore } from "../../../utils/data/store/store";
+
+  function convertDecimalTimeToReal(decimal) {
+    const minutes = Math.floor(decimal);
+    const seconds = Math.round((decimal - minutes) * 60);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  }
+</script>
+
+<span>{convertDecimalTimeToReal($parametriLavorazioneStore.tempo.value)}</span>
+
